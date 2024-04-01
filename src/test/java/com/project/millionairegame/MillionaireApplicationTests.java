39abfdee32061b1a45a2bb356592291386899b78
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-04-01 08:49:29 -0500

    First commit of 04/01

---
+++ src/test/java/com/project/millionairegame/MillionaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.millionairegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
