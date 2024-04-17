f27adb25b598de6b12967051a3892bd2cacf0112
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-04-17 13:54:33 -0500

    Second commit of 04/17
    Removed testing lines in MillionaireApplication class

--- src/backend/main/java/com/project/millionairegame/MillionaireApplication.java
+++ src/backend/main/java/com/project/millionairegame/MillionaireApplication.java
@@ -1,27 +1,13 @@
 package backend.main.java.com.project.millionairegame;
 
-import backend.main.java.com.project.millionairegame.pojo.Question;
-import backend.main.java.com.project.millionairegame.service.QuestionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class MillionaireApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MillionaireApplication.class, args);
-
-		QuestionService questionService = new QuestionService();
-
-		try {
-			Question newQuestion = questionService.getNewQuestion("500000");
-			System.out.println(newQuestion);
-
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
 	}
 
 }
