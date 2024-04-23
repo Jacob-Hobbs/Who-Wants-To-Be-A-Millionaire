eb05b590b2074ab616b972734aee59810ccdce19
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-04-23 16:18:58 -0500

    First commit of 04/23
    Tinkering with levelAside

--- src/frontend/javascript/index.js
+++ src/frontend/javascript/index.js
@@ -1,13 +1,29 @@
 document.getElementById("popoutButton").addEventListener("click", function() {
-    toggleAside();
+    showAside();
 });
 
-//FIXME: This should toggle the aside of the main page
-function toggleAside() {
+let asideOpen = false;
+
+//function to show aside
+function showAside() {
     const aside = document.getElementById("levelAside");
-    console.log("SEEN!");
-    if (aside.style.width === "0px") {
-        aside.style.width = "250px";
-        aside.style.visibility = "visible";
-    }
+    const questionScreen = document.getElementById("questionScreen");
+    asideOpen = true;
+    aside.style.width = "250px";
+    aside.style.visibility = "visible";
+    questionScreen.style.zIndex = "1";
+
+
+
+    // aside.style.width = "250px";
+    // aside.style.visibility = "visible";
+    // aside.style.transition = "width 0.8s ease";
+    // aside.style.zIndex = "2";
+}
+
+// function to hide aside
+function hideAside() {
+    const aside = document.getElementById("levelAside");
+    aside.style.width = "0px";
+    aside.style.visibility = "none";
 }
